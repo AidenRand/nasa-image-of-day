59b8932ed612c4f2329e4a250e258281a00d91e7
aiden <aidenrphotos@gmail.com>
2023-03-15 11:33:15 -0400

    Return user input on button press

--- main.py
+++ main.py
@@ -6,16 +6,24 @@ root.geometry("800x800")
 root.title("Nasa Images")
 
 
+input_font = font.Font(size=16)
+user_input = Entry(root, width=30, font=input_font)
+user_input.place(x=180, y=20, height=45)
+
+
 def get_user_input():
-    input_font = font.Font(size=16)
-    user_input = Entry(root, width=30, font=input_font)
-    user_input.place(x=180, y=20, height=45)
-
-    enter_input = Button(root, text="🔍", font=input_font, height=1)
-    enter_input.place(x=560, y=20)
+    try:
+        return user_input.get()
+    finally:
+        user_input.delete(0, END)
 
 
-get_user_input()
+def get_image():
+    get_user_input()
+
+
+enter_input = Button(root, text="🔍", font=input_font, height=1, command=get_image)
+enter_input.place(x=560, y=20)
 
 
 root.mainloop()
