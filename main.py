05886c3248bd40fdd845c3023905fea0c0df5814
aiden <aidenrphotos@gmail.com>
2023-03-20 12:29:31 -0400

    Print image explanation

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import requests
 import json
 
 root = Tk()
-root.geometry("1500x1000")
+root.geometry("1900x1080")
 root.title("Nasa Images")
 root.config(bg="#d8d8d8")
 
@@ -15,11 +15,18 @@ def get_image():
     url = "https://api.nasa.gov/planetary/apod?api_key=tgdFlsc0ek07wdiX6UI5RDFH0793AzMD5YQi0DWE"
 
     params = {"hd": "True"}
+    params2 = {"explanation": "True"}
 
     # get json data from api
     response = requests.get(url, params=params)
     json_data = json.loads(response.text)
     image_url = json_data["url"]
+
+    # Print image explanation
+    explanation_font = font.Font(size=14)
+    explanation = Text(root, width=130, height=8, wrap=WORD, font=explanation_font)
+    explanation.insert(INSERT, json_data["explanation"])
+    explanation.place(x=250, y=770)
     return image_url
 
 
@@ -37,8 +44,8 @@ def show_image():
 
 
 buttonFont = font.Font(size=16)
-imageBtn = Button(root, text="Get image", font=buttonFont, command=show_image)
-imageBtn.place(x=680, y=10)
+imageBtn = Button(root, text="Get image", font=buttonFont, command=get_image)
+imageBtn.place(x=880, y=19)
 
 # Add scrollbar
 sb = Scrollbar(root)
