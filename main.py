37bdf80831b40d081c7b50974fc1a8b1490f7156
aiden <aidenrphotos@gmail.com>
2023-03-23 12:04:14 -0400

    Get widgets to show

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from tkinter import *
 from tkinter import font
-from tkcalendar import DateEntry
+from tkinter import ttk
 from PIL import ImageTk
 from urllib.request import urlopen
 import requests
@@ -11,24 +11,48 @@ root.geometry("1900x1080")
 root.title("Nasa Images")
 root.config(bg="#d8d8d8")
 
+# Add a main_frame
+main_frame = Frame(root)
+main_frame.pack(fill=BOTH, expand=1)
+
+# Create a canvas
+page_canvas = Canvas(main_frame)
+page_canvas.pack(side=LEFT, fill=BOTH, expand=1)
+
+# Add page scrollbar
+page_scrollbar = ttk.Scrollbar(main_frame, orient=VERTICAL, command=page_canvas.yview)
+page_scrollbar.pack(side=RIGHT, fill=Y)
+
+# Configure canvas
+page_canvas.configure(yscrollcommand=page_scrollbar.set)
+page_canvas.bind(
+    "<Configure>", lambda e: page_canvas.configure(scrollregion=page_canvas.bbox("all"))
+)
+
+# Create another frame in canvas
+second_frame = Frame(page_canvas)
+
+# Add new frame to a window in the canvas
+page_canvas.create_window((0, 0), window=second_frame, anchor="nw")
+
 
 # Create date entries
 entry_font = font.Font(size=14)
 
-year_entry = Entry(root, width=6, font=entry_font)
-year_entry.place(x=700, y=25)
+year_entry = Entry(second_frame, width=6, font=entry_font)
+year_entry.grid(row=1, column=0)
 
-seperator1 = Label(root, text="-", font=entry_font)
+seperator1 = Label(second_frame, text="-", font=entry_font)
 seperator1.place(x=776, y=25)
 
-month_entry = Entry(root, width=3, font=entry_font)
-month_entry.place(x=790, y=25)
+month_entry = Entry(second_frame, width=3, font=entry_font)
+month_entry.grid(row=1, column=4)
 
-seperator1 = Label(root, text="-", font=entry_font)
-seperator1.place(x=834, y=25)
+seperator1 = Label(second_frame, text="-", font=entry_font)
+seperator1.grid(row=1, column=5)
 
-day_entry = Entry(root, width=3, font=entry_font)
-day_entry.place(x=850, y=25)
+day_entry = Entry(second_frame, width=3, font=entry_font)
+day_entry.grid(row=1, column=8)
 
 
 # Get image date
@@ -57,7 +81,7 @@ def get_image():
     # Print image explanation
     explanation_font = font.Font(size=14)
     explanation = Text(
-        root,
+        second_frame,
         width=40,
         height=27,
         wrap=WORD,
@@ -66,7 +90,7 @@ def get_image():
     )
     explanation.insert(INSERT, json_data["explanation"])
     explanation.config(state=DISABLED)
-    explanation.place(x=1400, y=80)
+    explanation.grid(row=10, column=10)
     print(get_date())
     return image_url
 
@@ -79,17 +103,18 @@ def show_image():
 
     # print photo to window
     image = ImageTk.PhotoImage(data=raw_data)
-    label = Label(image=image)
+    label = Label(second_frame, image=image)
     label.image = image
-    label.place(x=70, y=80)
+    label.grid(row=0, column=0)
 
 
 buttonFont = font.Font(size=16)
-imageBtn = Button(root, text="Get image", font=buttonFont, command=show_image)
-imageBtn.place(x=920, y=19)
+imageBtn = Button(
+    second_frame, text="Get image", font=buttonFont, command=show_image
+).grid(row=-1, column=100)
 
-# Add scrollbar
-sb = Scrollbar(root)
-sb.pack(side=RIGHT, fill=Y)
+
+my_label = Label(second_frame, text="hello").grid(row=3, column=2)
+
 
 root.mainloop()
