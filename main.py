d5b62b44cf6c6f64da99efd2cb11bb0e5c814653
aiden <aidenrphotos@gmail.com>
2023-03-25 15:12:29 -0400

    Set default date as current date

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from tkinter import *
 from tkinter import font
-from tkinter import ttk
 from PIL import ImageTk
 from urllib.request import urlopen
+from datetime import date
+import webbrowser
 import requests
 import json
 
@@ -11,48 +12,34 @@ root.geometry("1900x1080")
 root.title("Nasa Images")
 root.config(bg="#d8d8d8")
 
-# Add a main_frame
-main_frame = Frame(root)
-main_frame.pack(fill=BOTH, expand=1)
+# Get todays date
+today = date.today()
 
-# Create a canvas
-page_canvas = Canvas(main_frame)
-page_canvas.pack(side=LEFT, fill=BOTH, expand=1)
-
-# Add page scrollbar
-page_scrollbar = ttk.Scrollbar(main_frame, orient=VERTICAL, command=page_canvas.yview)
-page_scrollbar.pack(side=RIGHT, fill=Y)
-
-# Configure canvas
-page_canvas.configure(yscrollcommand=page_scrollbar.set)
-page_canvas.bind(
-    "<Configure>", lambda e: page_canvas.configure(scrollregion=page_canvas.bbox("all"))
-)
-
-# Create another frame in canvas
-second_frame = Frame(page_canvas)
-
-# Add new frame to a window in the canvas
-page_canvas.create_window((0, 0), window=second_frame, anchor="nw")
+year = today.strftime("%Y")
+month = today.strftime("%m")
+day = today.strftime("%d")
 
 
 # Create date entries
 entry_font = font.Font(size=14)
 
-year_entry = Entry(second_frame, width=6, font=entry_font)
-year_entry.grid(row=1, column=0)
+year_entry = Entry(root, width=6, font=entry_font)
+year_entry.place(x=700, y=25)
+year_entry.insert(0, year)
 
-seperator1 = Label(second_frame, text="-", font=entry_font)
+seperator1 = Label(root, text="-", font=entry_font)
 seperator1.place(x=776, y=25)
 
-month_entry = Entry(second_frame, width=3, font=entry_font)
-month_entry.grid(row=1, column=4)
+month_entry = Entry(root, width=3, font=entry_font)
+month_entry.place(x=790, y=25)
+month_entry.insert(0, month)
 
-seperator1 = Label(second_frame, text="-", font=entry_font)
-seperator1.grid(row=1, column=5)
+seperator1 = Label(root, text="-", font=entry_font)
+seperator1.place(x=834, y=25)
 
-day_entry = Entry(second_frame, width=3, font=entry_font)
-day_entry.grid(row=1, column=8)
+day_entry = Entry(root, width=3, font=entry_font)
+day_entry.place(x=850, y=25)
+day_entry.insert(0, day)
 
 
 # Get image date
@@ -79,9 +66,12 @@ def get_image():
     image_url = json_data["url"]
 
     # Print image explanation
+    erase_image = Label(root, width=200, height=200)
+    erase_image.place(x=1320, y=0)
+
     explanation_font = font.Font(size=14)
     explanation = Text(
-        second_frame,
+        root,
         width=40,
         height=27,
         wrap=WORD,
@@ -90,8 +80,7 @@ def get_image():
     )
     explanation.insert(INSERT, json_data["explanation"])
     explanation.config(state=DISABLED)
-    explanation.grid(row=10, column=10)
-    print(get_date())
+    explanation.place(x=1420, y=150)
     return image_url
 
 
@@ -101,20 +90,19 @@ def show_image():
     raw_data = u.read()
     u.close()
 
+    # Create image background to erase old image
+    erase_image = Label(root, width=200, height=200)
+    erase_image.place(x=0, y=65)
+
     # print photo to window
     image = ImageTk.PhotoImage(data=raw_data)
-    label = Label(second_frame, image=image)
+    label = Label(image=image)
     label.image = image
-    label.grid(row=0, column=0)
+    label.place(x=15, y=65)
 
 
 buttonFont = font.Font(size=16)
-imageBtn = Button(
-    second_frame, text="Get image", font=buttonFont, command=show_image
-).grid(row=-1, column=100)
-
-
-my_label = Label(second_frame, text="hello").grid(row=3, column=2)
-
+imageBtn = Button(root, text="Get image", font=buttonFont, command=show_image)
+imageBtn.place(x=920, y=19)
 
 root.mainloop()
